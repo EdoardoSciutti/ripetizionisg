15ea67af6b9c6c672ecba9f153ece729a756a08e
Sciubby <147152536+EdoardoSciutti@users.noreply.github.com>
2025-01-08 19:27:32 +0100

    push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,25 +13,25 @@ function Home() {
                 <Accordion defaultActiveKey="0" className="w-100">
                     <Accordion.Item eventKey="0">
                         <Accordion.Header>Esperienza diretta</Accordion.Header>
-                        <Accordion.Body>
+                        <Accordion.Body className="accordion-body">
                             Essendo studenti, conosciamo bene le sfide che stai affrontando. Possiamo aiutarti a superare le tue difficoltà con metodi di studio che abbiamo già testato e utilizzato con successo.
                         </Accordion.Body>
                     </Accordion.Item>
                     <Accordion.Item eventKey="1">
                         <Accordion.Header>Approccio personalizzato</Accordion.Header>
-                        <Accordion.Body>
+                        <Accordion.Body className="accordion-body">
                             Ogni lezione è progettata su misura per te, in base alle tue specifiche esigenze e al tuo stile di apprendimento. Che tu abbia bisogno di aiuto con i compiti, di prepararti per un esame o di migliorare in una materia specifica, siamo qui per supportarti!
                         </Accordion.Body>
                     </Accordion.Item>
                     <Accordion.Item eventKey="2">
                         <Accordion.Header>Accessibilità e flessibilità</Accordion.Header>
-                        <Accordion.Body>
+                        <Accordion.Body className="accordion-body">
                             Le nostre ripetizioni sono accessibili e flessibili. Offriamo lezioni sia online che in presenza, per adattarci al meglio ai tuoi impegni e alle tue preferenze.
                         </Accordion.Body>
                     </Accordion.Item>
                     <Accordion.Item eventKey="3">
                         <Accordion.Header>Ambiente amichevole</Accordion.Header>
-                        <Accordion.Body>
+                        <Accordion.Body className="accordion-body">
                             Crediamo che l'apprendimento debba essere un'esperienza positiva e stimolante. Imparerai in un ambiente accogliente e privo di giudizi, dove potrai sentirti libero di fare domande e migliorare.
                         </Accordion.Body>
                     </Accordion.Item>
