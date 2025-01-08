8dd9a0952b51bcc2160a6d3d9d37cdf36b5e6acd
Sciubby <147152536+EdoardoSciutti@users.noreply.github.com>
2025-01-08 19:31:07 +0100

    ah 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,32 +6,32 @@ function Home() {
     return (
         <>
             <Navbar />
-            <div className="container main-content" style={{ position: 'relative' }}>
+            <div className="container main-content" style={{ position: 'relative', paddingBottom: '20vh'}}>
                 <h1>Ripetizioni da studenti per studenti</h1>
                 <p>Siamo un gruppo di studenti appassionati che credono nel valore della collaborazione e dell'apprendimento condiviso. Offriamo ripetizioni per studenti di tutte le età e livelli scolastici, con un approccio giovane e dinamico che ci consente di comprendere al meglio le tue esigenze e difficoltà.</p>
                 <p>Perché scegliere noi?</p>
                 <Accordion defaultActiveKey="0" className="w-100">
                     <Accordion.Item eventKey="0">
                         <Accordion.Header>Esperienza diretta</Accordion.Header>
-                        <Accordion.Body className="accordion-body">
+                        <Accordion.Body>
                             Essendo studenti, conosciamo bene le sfide che stai affrontando. Possiamo aiutarti a superare le tue difficoltà con metodi di studio che abbiamo già testato e utilizzato con successo.
                         </Accordion.Body>
                     </Accordion.Item>
                     <Accordion.Item eventKey="1">
                         <Accordion.Header>Approccio personalizzato</Accordion.Header>
-                        <Accordion.Body className="accordion-body">
+                        <Accordion.Body>
                             Ogni lezione è progettata su misura per te, in base alle tue specifiche esigenze e al tuo stile di apprendimento. Che tu abbia bisogno di aiuto con i compiti, di prepararti per un esame o di migliorare in una materia specifica, siamo qui per supportarti!
                         </Accordion.Body>
                     </Accordion.Item>
                     <Accordion.Item eventKey="2">
                         <Accordion.Header>Accessibilità e flessibilità</Accordion.Header>
-                        <Accordion.Body className="accordion-body">
+                        <Accordion.Body>
                             Le nostre ripetizioni sono accessibili e flessibili. Offriamo lezioni sia online che in presenza, per adattarci al meglio ai tuoi impegni e alle tue preferenze.
                         </Accordion.Body>
                     </Accordion.Item>
                     <Accordion.Item eventKey="3">
                         <Accordion.Header>Ambiente amichevole</Accordion.Header>
-                        <Accordion.Body className="accordion-body">
+                        <Accordion.Body>
                             Crediamo che l'apprendimento debba essere un'esperienza positiva e stimolante. Imparerai in un ambiente accogliente e privo di giudizi, dove potrai sentirti libero di fare domande e migliorare.
                         </Accordion.Body>
                     </Accordion.Item>
