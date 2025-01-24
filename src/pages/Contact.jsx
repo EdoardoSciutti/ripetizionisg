08ab638333d26e87da0a60bb4b164e0723df4747
Sciubby <147152536+EdoardoSciutti@users.noreply.github.com>
2025-01-24 18:03:29 +0100

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,7 @@ function Contact() {
                 <h1>Contatti</h1>
                 <p style={{ paddingTop: '10%' }}>Per qualsiasi necessità, potete contattarci attraverso i seguenti canali:</p>
                 <p>Telefono (Solo Whatsapp):</p>
-                <p>3478675903 / 3476581234</p>
+                <p>3478675903 / 3472809838</p>
             </div>
         </>
     );
